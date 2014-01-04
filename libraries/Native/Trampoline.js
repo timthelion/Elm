fe6d9f364e86f331eb14518e369fc73d557e15b7
Max New <maxsnew@gmail.com>
2014-01-04 04:35:23 -0600

    Use maxnew's native Trampoline support for unbounded recursion
    Conflicts:
    	libraries/Native/Trampoline.js

--- libraries/Native/Trampoline.js
+++ libraries/Native/Trampoline.js
@@ -4,16 +4,22 @@ Elm.Native.Trampoline.make = function(elm) {
     elm.Native.Trampoline = elm.Native.Trampoline || {};
     if (elm.Native.Trampoline.values) return elm.Native.Trampoline.values;
 
-    function trampoline(thunkstack,next){
-     var next_ts;
-     while((next_ts=next(thunkstack)) && next_ts.ctor=="Left"){
-      thunkstack=next_ts._0;
-     }//Loop ends when next_ts is Right
-     return next_ts._0;
+    var _E = Elm.Native.Error.make(elm),
+
+    // trampoline : Trampoline a -> a
+    trampoline = function(t) {
+        var tramp = t;
+        while(true) {
+            switch(tramp._0.ctor) {
+            case "Left":
+                return tramp._0._0;
+            case "Right":
+                tramp = tramp._0._0({ctor: "_Tuple0"});
+                continue;
+            }
+            _E.Case("Trampoline", "in Native.Trampoline.trampoline");
+        }
     }
 
-    return elm.Native.Trampoline.values = {
-        trampoline: F2(trampoline),
-    };
-    
-};
+    return elm.Native.Trampoline.values = { trampoline: trampoline };
+}
